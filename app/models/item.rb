6b08a1c9c9d678211cfcb821e631b0ea4705fd17
katsuya53 <redstar53hk@gmail.com>
2024-02-08 17:01:12 +0900

    テストコード追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, class_name: 'Category'
-  belongs_to :sales_status, class_name: 'Sales_status' # クラス名を付けないと出品createアクションでエラーとなることがある
-  belongs_to :shipping_fee_status, class_name: 'Shipping_fee_status'
-  belongs_to :prefecture, class_name: 'Prefecture'
-  belongs_to :scheduled_delivery, class_name: 'Scheduled_delivery'
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
   belongs_to :user
   has_one_attached :image
