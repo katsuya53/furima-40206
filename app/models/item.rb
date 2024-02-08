bda6023267e8be52749f3a4a1f76db0d228b236c
katsuya53 <redstar53hk@gmail.com>
2024-02-08 13:52:08 +0900

    商品出品機能を実装(テストコード無)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, class_name: 'Category'
+  belongs_to :sales_status, class_name: 'Sales_status' # クラス名を付けないと出品createアクションでエラーとなることがある
+  belongs_to :shipping_fee_status, class_name: 'Shipping_fee_status'
+  belongs_to :prefecture, class_name: 'Prefecture'
+  belongs_to :scheduled_delivery, class_name: 'Scheduled_delivery'
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: true
+  validates :image, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :items
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,10 +7,15 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birth_date, presence: true
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "is invalid. Include both letters and numbers" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " is invalid. Input full-width characters" }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " is invalid. Input full-width characters" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,message: " is invalid. Input full-width katakana characters" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,message: " is invalid. Input full-width katakana characters" }
-  
+  validates :password,
+            format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+                      message: 'is invalid. Include both letters and numbers' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters' }
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters' }
 end
