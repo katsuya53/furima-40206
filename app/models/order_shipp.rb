bd67ae8be35a86c923d5f6a99191d2c7d6fe86fc
katsuya53 <redstar53hk@gmail.com>
2024-02-13 13:01:01 +0900

    手数料計算表記、電話番号エラーメッセージ重複の修正

--- app/models/order_shipp.rb
+++ app/models/order_shipp.rb
@@ -5,10 +5,8 @@ class OrderShipp
   with_options presence: true do
     validates :user_id, :item_id, :city, :addresses, :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. Input only numbers.' }
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: lambda { |_object, data|
-                                                                         'is too short' if data[:value].to_s.size < 10
-                                                                       } }
+    validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. Input only numbers.' },
+                             length: { within: 10..11, message: 'is too short' }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :building, allow_nil: true, length: { maximum: 255 }
