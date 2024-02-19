f0ca4be11c3afb690805da722b9c24ae397e0ae2
katsuya53 <redstar53hk@gmail.com>
2024-02-20 01:24:05 +0900

    コメント機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one :order
+  has_many :comments
 
   validates :name, presence: true
   validates :info, presence: true
